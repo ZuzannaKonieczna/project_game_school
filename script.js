791d31f123de3d8341cd0684783da88e478e52ae
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-03-10 22:42:51 +0100

    adding html/js/css

---
+++ script.js
@@ -0,0 +1,8 @@
+function ilos(){
+    var slowo = document.getElementById("sl").value;
+    var dl = slowo.length;
+    alert (dl);
+   document.getElementById("slowo").innerHTML=<table><tr>
+   document.getElementById("slowo").innerHTML=<td> </td>
+   document.getElementById("slowo").innerHTML=</tr></table>
+}
