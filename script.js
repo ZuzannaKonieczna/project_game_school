9aeffc572e6f19b3997220ddefd001ee8ff1b6ef
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-03-12 12:33:53 +0100

    some changes and adding div :)

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function ilos(){
     var slowo = document.getElementById("sl").value;
     var dl = slowo.length;
+    var p = dl;
     alert (dl);
-    for (var i = 0; i <= dl; i++){
-        document.getElementById("dw").innerText = "_";
+    for (var i = 0; i <= p; i++){
+        console.log("_");
     }
     
 }
